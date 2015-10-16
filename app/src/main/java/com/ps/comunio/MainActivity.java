035dc6f908f89acf415274eff8f71c7d45199cf2
ivan <yvangomezalonso@gmail.com>
2015-10-16 14:44:58 +0200

    Añadido función para cambiar de layout y menu desplegable a la derecha.

--- app/src/main/java/com/ps/comunio/MainActivity.java
+++ app/src/main/java/com/ps/comunio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ps.comunio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.ps.comunio.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-    }
 
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,LoggedIn.class);
+        EditText usuario = (EditText) findViewById(R.id.username);
+        String strUsuario = usuario.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,strUsuario);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
