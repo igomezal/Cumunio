4dcb524da7d2ac575ee7724c160bb243d396f86f
sergiownd <sergiownd94@gmail.com>
2015-10-25 11:55:33 +0100

    Prueba de espacio

--- app/src/main/java/com/ps/comunio/DatosJugador.java
+++ app/src/main/java/com/ps/comunio/DatosJugador.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 public class DatosJugador extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
