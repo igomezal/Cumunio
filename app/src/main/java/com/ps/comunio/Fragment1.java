06680c53df5aeb76e7670178a2a79cc10d3e77a7
Jorge Galindo Peña <jorgape225@gmail.com>
2015-11-01 20:12:42 +0100

    Añadido imagenes de los jugadores y fichajes v3.0

--- app/src/main/java/com/ps/comunio/Fragment1.java
+++ app/src/main/java/com/ps/comunio/Fragment1.java
@@ -2,43 +2,40 @@ package com.ps.comunio;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Fragment1 extends ListFragment {
 
     private Jugador[] datos={
-            new Jugador("Keylor Navas","Real Madrid","Portero","7000000"),
-            new Jugador("Lucas Perez","Deportivo","Centrocampista","11250000"),
-            new Jugador("Nolito","Celta","Delantero","15320000"),
-            new Jugador("Orellana","Celta","Centrocampista","14190000"),
-            new Jugador("Iago Aspas","Celta","Delantero","11040000"),
-            new Jugador("Cristiano Ronaldo","Real Madrid","Delantero","22910000"),
-            new Jugador("Benzema","Real Madrid","Delantero","14510000"),
-            new Jugador("Modric","Real Madrid","Centrocampista","14300000"),
-            new Jugador("Marcelo","Real Madrid","Defensa","8450000"),
-            new Jugador("Saúl Berjón","Eibar","Centrocampista","6680000"),
-            new Jugador("Daniel Wass","Celta","Centrocampista","6540000"),
-            new Jugador("Griezmann","Atlético de Madrid","Delantero","21560000"),
-            new Jugador("Westermann","Betis","Defensa","5190000"),
-            new Jugador("Jonathan Viera","Las Palmas","Centrocampista","7060000"),
-            new Jugador("Norja Bastón","Eibar","Delantero","6800000"),
-            new Jugador("Lora","Sporting","Defensa","4070000"),
-            new Jugador("Luis Suárez","Futbol Club Barcelona","Delantero","17690000"),
-            new Jugador("Augusto Fernández","Celta","Centrocampista","5640000"),
-            new Jugador("Sergi Roberto","Futbol Club Barcelona","Centrocampista","4420000"),
-            new Jugador("David Simón","Las Palmas","Defensa","3910000"),
-            new Jugador("Aduriz","Athletic Bilbao","Delantero","12420000"),
-            new Jugador("Rubén Castro","Betis","Delantero","9500000")
+            new Jugador("Keylor Navas","Real Madrid","Portero","7000000", R.drawable.navas),
+            new Jugador("Lucas Perez","Deportivo","Centrocampista","11250000", R.drawable.lucas),
+            new Jugador("Nolito","Celta","Delantero","15320000", R.drawable.nolito),
+            new Jugador("Orellana","Celta","Centrocampista","14190000", R.drawable.orellana),
+            new Jugador("Cristiano Ronaldo","Real Madrid","Delantero","22910000", R.drawable.cristiano),
+            new Jugador("Benzema","Real Madrid","Delantero","14510000", R.drawable.benzema),
+            new Jugador("Modric","Real Madrid","Centrocampista","14300000", R.drawable.modric),
+            new Jugador("Marcelo","Real Madrid","Defensa","8450000", R.drawable.marcelo),
+            new Jugador("Saúl Berjón","Eibar","Centrocampista","6680000",R.drawable.saul),
+            new Jugador("Daniel Wass","Celta","Centrocampista","6540000", R.drawable.cristiano),
+            new Jugador("Griezmann","Atlético de Madrid","Delantero","21560000",R.drawable.wass),
+            new Jugador("Westermann","Betis","Defensa","5190000", R.drawable.westermann),
+            new Jugador("Jonathan Viera","Las Palmas","Centrocampista","7060000", R.drawable.viera),
+            new Jugador("Norja Bastón","Eibar","Delantero","6800000", R.drawable.defecto),
+            new Jugador("Lora","Sporting","Defensa","4070000", R.drawable.defecto),
+            new Jugador("Luis Suárez","Futbol Club Barcelona","Delantero","17690000", R.drawable.suarez),
+            new Jugador("Augusto Fernández","Celta","Centrocampista","5640000", R.drawable.augusto),
+            new Jugador("Sergi Roberto","Futbol Club Barcelona","Centrocampista","4420000", R.drawable.sergi),
+            new Jugador("David Simón","Las Palmas","Defensa","3910000", R.drawable.simon),
+            new Jugador("Aduriz","Athletic Bilbao","Delantero","12420000", R.drawable.aduriz),
+            new Jugador("Rubén Castro","Betis","Delantero","9500000", R.drawable.castro)
+
     };
 
     public Fragment1() {
@@ -79,6 +76,9 @@ public class Fragment1 extends ListFragment {
 
             TextView Nombre = (TextView) item.findViewById(R.id.NombreJugador);
             Nombre.setText(datos[position].getNombre());
+
+            ImageView Imagen = (ImageView) item.findViewById(R.id.ImagenJugador);
+            Imagen.setImageResource(datos[position].getImagen());
 
             return item;
         }
