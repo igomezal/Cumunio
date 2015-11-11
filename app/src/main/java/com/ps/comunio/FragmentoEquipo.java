3f9c8fb23f677b68818ae5806aae05597c5926f9
cruzmaster <sefi-sefi@hotmail.com>
2015-11-11 16:01:40 +0100

    Cartas en equipos V1 (mejorar)

--- app/src/main/java/com/ps/comunio/FragmentoEquipo.java
+++ app/src/main/java/com/ps/comunio/FragmentoEquipo.java
@@ -1,27 +1,37 @@
 package com.ps.comunio;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by sergiownd on 25/10/15.
  */
 public class FragmentoEquipo extends ListFragment {
     private Equipo[] datos={
-        new Equipo("Mantester Unido",150000000),
-        new Equipo("Real Mandril",180000000),
-        new Equipo("Cholsea",130000000),
-        new Equipo("Armético de Matriz",150000000),
-        new Equipo("Bayar de Manich",150000000),
+        new Equipo("Mantester Unido",150000000,R.drawable.manchester),
+        new Equipo("Real Mandril",180000000,R.drawable.madrid),
+        new Equipo("Cholsea",130000000,R.drawable.chelsea),
+        new Equipo("Armético de Matriz",150000000,R.drawable.atleti),
+        new Equipo("Bayar de Manich",150000000,R.drawable.munich),
     };
     public FragmentoEquipo() {
         // Required empty public constructor
@@ -50,6 +60,10 @@ public class FragmentoEquipo extends ListFragment {
 
             TextView Nombre = (TextView) item.findViewById(R.id.NombreEquipo);
             Nombre.setText(datos[position].getNombre());
+
+            ImageView Imagen = (ImageView) item.findViewById(R.id.ImagenEquipo);
+            Imagen.setImageResource(datos[position].getEqImagen());
+
 
             return item;
         }
