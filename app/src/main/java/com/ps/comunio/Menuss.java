b267ece21bacdd1bf486b5833681ec7eb5e4d164
igomezal <yvangomezalonso@gmail.com>
2015-11-29 22:33:30 +0100

    Revert "Mejora noticias"

--- app/src/main/java/com/ps/comunio/Menuss.java
+++ app/src/main/java/com/ps/comunio/Menuss.java
@@ -1,6 +1,7 @@
 package com.ps.comunio;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -9,8 +10,17 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Menuss extends AppCompatActivity {
     private Toolbar appbar;
