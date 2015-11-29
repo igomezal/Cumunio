12b260132fec6347476c92b7407a1e715c91e305
igomezal <yvangomezalonso@gmail.com>
2015-11-29 22:31:22 +0100

    Revert "Interfaz reglas mejorada"

--- app/src/main/java/com/ps/comunio/Noticia.java
+++ app/src/main/java/com/ps/comunio/Noticia.java
@@ -4,18 +4,22 @@ package com.ps.comunio;
  * Created by Mario on 16/11/2015.
  */
 public class Noticia {
-    private String titular, cuerpo;
-
-    public Noticia(String titular, String cuerpo) {
-        this.titular = titular;
-        this.cuerpo = cuerpo;
+    private String titular,cuerpo,fecha;
+    public Noticia(String titular, String cuerpo,String fecha){
+        this.titular=titular;
+        this.cuerpo=cuerpo;
+        this.fecha=fecha;
     }
 
-    public String getTitular() {
+    public String getTitular(){
         return this.titular;
     }
 
-    public String getCuerpo() {
+    public String getCuerpo(){
         return this.cuerpo;
+    }
+
+    public String getFecha(){
+        return this.fecha;
     }
 }
